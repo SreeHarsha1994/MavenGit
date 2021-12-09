18c5ea0adf87d4fe2ced925c0c958e6470754283
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2021-12-10 04:51:46 +0530

    New push...

--- src/test/java/GitHubParctice/GitHub.java
+++ src/test/java/GitHubParctice/GitHub.java
@@ -7,6 +7,7 @@ public class GitHub {
 	public void Demo() {
 		System.out.println("NEw Commit and Push");
 		System.out.println("Pushing to other account");
+		System.out.println("Pushing to other account 2nd");
 	}
 
 }
