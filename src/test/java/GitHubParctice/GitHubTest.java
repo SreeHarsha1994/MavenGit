705854ffe4eeb2b61b0a00dffd92f541d7154c39
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2021-12-10 14:23:58 +0530

    ho

--- src/test/java/GitHubParctice/GitHubTest.java
+++ src/test/java/GitHubParctice/GitHubTest.java
@@ -17,6 +17,7 @@ public class GitHubTest {
 		System.out.println("New con");
 		System.out.println("===+++===");
 		System.out.println("Pushing to other account 2nd");
+		System.out.println("github");
 		
 	}
 
