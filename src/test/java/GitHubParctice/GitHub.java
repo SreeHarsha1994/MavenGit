50ac086c621b9c3227bed0125472dedb4a405fe3
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2021-12-10 06:55:05 +0530

    do

--- src/test/java/GitHubParctice/GitHub.java
+++ src/test/java/GitHubParctice/GitHub.java
@@ -5,9 +5,16 @@ import org.testng.annotations.Test;
 public class GitHub {
 	@Test
 	public void Demo() {
+		
+		String hungry="yes";
+
+		if(hungry=="yes") {
+			System.out.println("print");
+		}
 		System.out.println("NEw Commit and Push");
 		System.out.println("Pushing to other account");
 		System.out.println("Pushing to other account 2nd");
+		
 	}
 
 }
